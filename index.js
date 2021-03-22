acc8db9a309e244cbd35e265f5cfa38fd3718f0d
Damian <spearsda16@gmail.com>
2021-03-21 20:22:13 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = " Damian is the champion"
+
+
+
+
+
+
+
